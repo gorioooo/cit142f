aa7f545b4ecf424aa7bb94a96bbac5eb4240eedc
CIT142F-39@m307.uc-bcf.edu.ph <CIT142F-39@m307.uc-bcf.edu.ph>
2022-09-14 16:51:23 +0800

    -first upload

---
+++ naben.bat
@@ -0,0 +1,10 @@
+@echo off
+
+git remote set-url origin https://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@github.com/gorioooo/cit142f.git
+pause
+
+git add .
+git commit -m -"first upload"
+git push
+pause
+
