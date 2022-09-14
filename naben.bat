9f7f7af763514faf82e9c4c5624e69e280f311b4
CIT142F-39@M307.UC-BCF.EDU.PH <CIT142F-39@M307.UC-BCF.EDU.PH>
2022-09-14 17:06:16 +0800

    -first upload

--- naben.bat
+++ naben.bat
@@ -1,6 +1,6 @@
 @echo off
 
-git remote set-url origin https://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@github.com/gorioooo/cit142f.git
+git remote set-url origin https://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@github.com/gorioooo/cit142f.git
 pause
 
 git add .
